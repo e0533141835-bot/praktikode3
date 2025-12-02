961203b4692a1277c6c20dbc839068cb86d3768f
efrat <E0533141835@gmail.com>
2025-12-03 00:13:18 +0200

    caps_lock 1

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const apiUrl = "http://localhost:5278";
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.REACT_APP_API_URL || "https://todolist-server-k66i.onrender.com";
 
 
 export default {
